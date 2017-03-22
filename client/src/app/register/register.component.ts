8ca2efb11ab64257a3745b959f724be845dd1829
Dominik Finkbeiner <dominik@mathandoro.com>
2017-03-22 01:11:29 +0100

    register user form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable, Observer } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user = {
+    email: null,
+    firstname: null,
+    lastname: null,
+    password: null,
+    passwordRepeat: null
+  }
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
+  }
+
+  register(id: string) {
+    this.http.post(`/api/users/register`, this.user).map((res: any) => {
+      return res.json();
+    }).subscribe(() => {
+      alert("success")
+    })
   }
 
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -32,7 +32,9 @@ export class Server {
         app.engine('.hbs', hbs.engine)
         app.set('view engine', '.hbs')
 
-        app.use('/app', express.static(__dirname + '/../../client/dist'));
+        // app.use('/app/register', express.static(__dirname + '/../../client/dist/'));
+        app.use('/app', express.static(__dirname + '/../../client/dist/'));
+        app.use('/app/*', express.static(__dirname + '/../../client/dist/'));
         app.use('/static', express.static(__dirname + '/../../client/static'));
         app.use('/api', this.api.getRouter());
         app.use('/static', express.static(__dirname + '/../static'))
