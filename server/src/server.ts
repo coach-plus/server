08e8fde983ec9c21244027787a1825f3804978ee
Maurice Breit <maurice.breit@sap.com>
2017-05-11 14:43:29 +0200

    file size limit

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ export class Server {
     public start() {
         let app = express();
         let server = http.createServer(app);
-        app.use(bodyParser.json({ limit: '5mb' }));
+        app.use(bodyParser.json({ limit: '10mb' }));
 
         let hbs = exphbs.create({
             extname: '.hbs',
